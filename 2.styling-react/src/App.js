7aedb96380794be6e6120dec15ace5220ad4eeae
juntae <rlawnsxo131@gmail.com>
2018-11-10 00:49:50 +0900

    20181110 am12:49 styling-react

--- 2.styling-react/src/App.js
+++ 2.styling-react/src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classNames from 'classnames/bind';
+import styles from './App.module.css';
+
+const cx = classNames.bind(styles);
 
 class App extends Component {
   render() {
+    const isBlue = true;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={cx('box', {
+        blue: isBlue})}>
       </div>
     );
   }
