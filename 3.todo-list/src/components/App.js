5f9eed20b5ec54408fca3546a05924332f86eb9b
juntae <rlawnsxo131@gmail.com>
2018-11-14 01:51:41 +0900

    add pageTemplate for todo-list 20181113

---
+++ 3.todo-list/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PageTemplate from './PageTemplate';
+
+class App extends Component {
+    render() {
+        return(
+            <div>
+                <PageTemplate>안녕하세요</PageTemplate>                
+            </div>
+        )
+    }
+}
+
+export default App;
+
