023ba5865d8bd057ea0332159f7c5dd3699864dc
juntae <rlawnsxo131@gmail.com>
2018-11-29 22:57:29 +0900

    add react-router-tutorial

---
+++ 6.react-router-tutorial/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import {
+  Home,
+  About,
+} from 'pages';
+
+const App = () => {
+  return (
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route path="/about/:name?" component={About} />
+    </div>
+  )
+}
+
+export default App;
---
+++ 6.react-router-tutorial/src/pages/index.js
@@ -0,0 +1,6 @@
+/*
+다음 코드는 컴포넌트를 불러온 후
+동일한 이름으로 내보낸다.
+*/
+export { default as Home } from './Home';
+export { default as About } from './About';
