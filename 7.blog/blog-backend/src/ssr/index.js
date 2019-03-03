cc0aee7d94b3ba32bbcd0d645e68e1744dfad240
juntae <rlawnsxo131@gmail.com>
2019-03-03 15:11:03 +0900

    7.blog project serverside rednering Modified

--- 7.blog/blog-backend/src/ssr/index.js
+++ 7.blog/blog-backend/src/ssr/index.js
@@ -1,9 +1,18 @@
 //require('browser-env')();
 const render = require('./render').default;
+const fs = require('fs');
 
+const indexHtml = fs.readFileSync(
+  path.resolve(__dirname, render),
+  'utf8'
+);
 
+function createPage(rootHtml, bundles, state) {
+  let html = indexHtml;
 
-module.exports = async (ctx) => {
-  const rendered = render(ctx);
-  ctx.body = rendered;
-};
+  //스플리팅된 자바스크립트
+  const chunkScripts = bundles
+    .filter(bundle => bundle.file.match(/.js$/))
+    .map(bundle => `<script src="${bundle.publickPath}"></script>`)
+    .join('\n');
+}
