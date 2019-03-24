6b7e353b7ad788f6e843abf879879e241ea117b1
juntae <rlawnsxo131@gmail.com>
2019-03-24 18:16:11 +0900

    before yarn eject..

---
+++ 0.react-login/frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { Home, Auth } from 'pages'; 
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/auth" component={Auth} />
+      </div>
+    );
+  }
+}
+
+export default App;
