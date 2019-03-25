8f7948f9244668dc47c2b402cccb125c3d980760
juntae <rlawnsxo131@gmail.com>
2019-03-25 17:12:21 +0900

    add styled-components

---
+++ 2.styled-components/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import StyledComponent from './StyledComponent';
+
+class App extends Component {
+  render() {
+    return <StyledComponent />;
+  }
+}
+
+export default App;
